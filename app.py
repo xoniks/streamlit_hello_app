56a8e8dd04f14d251d0aabb4e29319a120125e00
e.baruti <63745689+xoniks@users.noreply.github.com>
2022-11-26 13:24:35 +0000

    time now added

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import streamlit as st
-from datetime import date 
+from datetime import date, datetime
 
 st.title("Hello Streamlit App")
 
 st.header("My name is egezon and this is my fisrt deployed app")
 
 today = date.today()
+time_now = datetime.now()
 st.write(today)
+st.write(time_now)
